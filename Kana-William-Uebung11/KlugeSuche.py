bdc2707710fb6f2e96bfa3b3b295c641fedcd72c
Boby024 <william.kana-tsoplefack@stud.uni-due.de>
2019-07-01 18:14:04 +0200

    search (KlugeSuche) function in a list

---
+++ Kana-William-Uebung11/KlugeSuche.py
@@ -0,0 +1,24 @@
+
+def suche(liste,a):
+    try:
+        return liste.index(a)
+    except ValueError:
+        return -1
+
+def sucheIndex(liste, a):
+    for i in range(a, len(liste)):
+        if(liste[i] ==a ):
+            return i
+    return -1
+
+
+if __name__ == '__main__':
+    my_liste = [2, 9, 5, 6, 1, 0]
+    zahl=1
+    print("The element exists in the Liste and the index is: ", sucheIndex(my_liste,1 ) ,"\n")
+    print("The element don't exist in the Liste : ", sucheIndex(my_liste, 3), "\n")
+
+    if( sucheIndex(my_liste, zahl)== -1 ):
+        print("resultat: ", -1)
+    else:
+        print( "resultat: ", sucheIndex(my_liste,zahl))
